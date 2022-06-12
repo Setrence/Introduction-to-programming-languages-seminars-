da088abd3b43944637bd162b5ec84bca21f3443e
Evgeniy <241126@mail.ru>
2022-06-12 22:25:51 +0300

    Add Dop_HomeWork

---
+++ Dop_HomeWork_001/Program.cs
@@ -0,0 +1,22 @@
+﻿// В переменной string есть секретное сообщение, во второй есть пароль. Пользователю программы дается 3 попытки ввести пароль и увидеть секретное сообщение.
+
+string message = "Поздравляю, ты угадал пароль";
+string password = "753951Qwe";
+string code;
+int i = 0;
+
+while (i < 3)
+{
+    Console.WriteLine("Введите пароль: ");
+    code = Console.ReadLine();
+    if (code == password)
+    {
+        Console.WriteLine(message);
+        i = 3;
+    }
+    else
+    {
+        Console.WriteLine("Неверный пароль");
+    }
+    i++;
+}

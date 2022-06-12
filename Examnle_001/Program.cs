15874708c90231cd5de79340b9e0020a0f7722f9
Evgeniy <241126@mail.ru>
2022-06-12 16:02:13 +0300

    Add program

--- Examnle_001/Program.cs
+++ Examnle_001/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число: ");
+string input = Console.ReadLine();
+int number = Convert.ToInt32(input);
+
+int result = number * number;
+
+Console.WriteLine(number + " в квадрате будет " + result);

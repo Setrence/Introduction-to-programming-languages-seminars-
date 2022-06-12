ce7acd35e29c61e9ed0a3cec657143a2e0431a8d
Evgeniy <241126@mail.ru>
2022-06-12 16:27:09 +0300

    Add Example_002

---
+++ Example_002/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Input number 1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input number 2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 * number1 == number2)
+    Console.WriteLine(number1 + "^2" + "=" + number2 + " (True) ");
+else
+    Console.WriteLine(number1 + "^2" + "=" + number2 + " (False) ");

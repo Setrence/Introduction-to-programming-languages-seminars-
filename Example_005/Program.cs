d3151b3aea7d5316d907ea20d70b9934f7b7ce56
Evgeniy <241126@mail.ru>
2022-06-12 17:25:03 +0300

    Add Example_005

---
+++ Example_005/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 99 && number < 1000)
+{
+    int lastNumber = number % 10;
+    Console.WriteLine("Последняя цифра вашего числа: " + lastNumber);
+}
+else
+{
+    Console.WriteLine("Вы ввели неверное число");
+}

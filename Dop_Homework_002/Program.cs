9f1a809656b99c45df892af951c38654cc8aa4df
Evgeniy <241126@mail.ru>
2022-06-12 22:38:56 +0300

    Add Dop_HomeWork_002

---
+++ Dop_Homework_002/Program.cs
@@ -0,0 +1,21 @@
+﻿// Есть программа с бесконечным циклом. Когда пользователь вводит exit программа заканчивается
+
+string message = "Вы вышли из цикла";
+string password = "exit";
+string code;
+int i = 1;
+
+while (i > 0)
+{
+    Console.WriteLine("Введите пароль: ");
+    code = Console.ReadLine();
+    if (code == password)
+    {
+        Console.WriteLine(message);
+        i = 0;
+    }
+    else
+    {
+        Console.WriteLine("Пароль неверный, цикл продолжается");
+    }
+}

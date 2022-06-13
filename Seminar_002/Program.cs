af5a18bd4e91f2f8c4a81ad88a0f6fae02d4522c
Evgeniy <241126@mail.ru>
2022-06-13 20:40:44 +0300

    Add Seminar_002

---
+++ Seminar_002/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите номер четверти: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number == 1)
+{
+    Console.WriteLine("По х: от 0 до + бесконечности");
+    Console.WriteLine("По y: от 0 до + бесконечности");
+}
+if (number == 2)
+{
+    Console.WriteLine("По х: от - бесконечности до 0");
+    Console.WriteLine("По y: от 0 до + бесконечности");
+}
+if (number == 3)
+{
+    Console.WriteLine("По х: от - бесконечности до 0");
+    Console.WriteLine("По y: от - бесконечности до 0");
+}
+if (number == 4)
+{
+    Console.WriteLine("По х: от 0 до бесконечности");
+    Console.WriteLine("По y: от - бесконечности до 0");
+}

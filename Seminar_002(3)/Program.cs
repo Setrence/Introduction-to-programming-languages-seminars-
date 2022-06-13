8de056fa8b857bb568372a1c5f887b94c2fc09f6
Evgeniy <241126@mail.ru>
2022-06-13 21:30:54 +0300

    Add Seminar_002(3)

---
+++ Seminar_002(3)/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите число: ");
+int min = 1;
+int N = Convert.ToInt32(Console.ReadLine());
+
+while (N >= min)
+{
+    Console.WriteLine(Metod(min));
+    min++;
+}
+
+
+
+
+
+
+int Metod (int N)
+{
+    return N*N;
+}
+

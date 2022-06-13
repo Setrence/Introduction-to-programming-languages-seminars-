b6c52d80802b70746b5ddc5680b06a597aecc213
Evgeniy <241126@mail.ru>
2022-06-13 21:04:10 +0300

    Add Seminar_002(2)

---
+++ Seminar_002(2)/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите координату x: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату x: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату y: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату y: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+
+double distant = ((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1));
+Console.WriteLine(Math.Sqrt(distant));

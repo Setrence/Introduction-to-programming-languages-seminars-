ec310fde853ebc595e748306f415060b32de157b
Evgeniy <241126@mail.ru>
2022-06-13 21:56:03 +0300

    Add HomeWork_005

---
+++ HomeWork_005/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход число (N)
+// и выдает таблицу кубов чисел от 1 до N.
+
+
+Console.Write("Введите число: ");
+int min = 1;
+int N = Convert.ToInt32(Console.ReadLine());
+
+while (N > min)
+{
+    Console.Write($"{Metod(min)}, ");
+    min++;
+}
+
+Console.Write(N*N*N);
+
+
+
+
+int Metod (int N)
+{
+    return N*N*N;
+}

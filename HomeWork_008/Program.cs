ae8ee78ab19896d72bd5942e92f24e93e0c66aa6
Evgeniy <241126@mail.ru>
2022-06-14 22:49:55 +0300

    mist

--- HomeWork_008/Program.cs
+++ HomeWork_008/Program.cs
@@ -3,7 +3,7 @@
 Console.Write("Введите трехзначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-if (number > 100 && number < 999)
+if (number >= 100 && number < 999)
 {
     int number2 = Metod(number) / 10;
     Console.WriteLine($"Вторая цифра числа {number} будет: {number2}");

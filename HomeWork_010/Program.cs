98df5f050eb97d48a113651466fd59f712eafa39
Evgeniy <241126@mail.ru>
2022-06-14 17:46:07 +0300

    Add HomeWork_010

---
+++ HomeWork_010/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.Write("Введите день недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 1 && number <= 7)
+{
+    Console.WriteLine($"День недели: {Metod(number)}");
+}
+else
+{
+    Console.WriteLine("Такого дня недели нет");
+}
+
+
+string Metod (int number)
+{
+    if (number == 6)
+    {
+        return ("Выходной");
+    }
+    if (number == 7)
+    {
+        return ("Выходной");
+    }
+    else
+    {
+        return ("Рабочий");
+    }
+}

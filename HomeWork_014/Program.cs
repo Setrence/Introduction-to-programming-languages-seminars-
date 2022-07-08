13ebb6642c5032021841b30a2a4a5e3468bf32b8
Evgeniy <241126@mail.ru>
2022-07-08 14:02:31 +0300

    HomeWork_014

---
+++ HomeWork_014/Program.cs
@@ -0,0 +1,28 @@
+﻿int [] array = new int [4];
+int sumEven = 0;
+
+for (int i=0; i< array.Length; i++)
+{
+    array[i] = new Random(). Next(100, 1000);
+}
+
+
+PrintArray(array);
+
+void PrintArray(int[] array)
+{
+    for (int i=0; i< array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+for (int i=0; i< array.Length; i++)
+{
+    if(array[i] % 2 == 0)
+        sumEven = sumEven + 0;
+    else 
+        sumEven = sumEven + 1;    
+}
+Console.WriteLine($"Четных чисел в массиве = {sumEven}");

dc36c04e3ea95d8a2f1aed1cfdb01f0858b76df0
Evgeniy <241126@mail.ru>
2022-07-08 14:27:18 +0300

    Add sum

--- HomeWork_015/Program.cs
+++ HomeWork_015/Program.cs
@@ -1,7 +1,7 @@
 ﻿//Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
 
 int[] array = new int [6];
-int sumEven = 0;
+int sumOddNumber = 0;
 
 for (int i=0; i< array.Length; i++)
 {
@@ -22,8 +22,8 @@ void PrintArray(int[] array)
 for (int i=0; i< array.Length; i++)
 {
     if(i % 2 == 0)
-        sumEven = sumEven + 0;
+        sumOddNumber = sumOddNumber + 0;
     else 
-        sumEven = sumEven + array[i];    
+        sumOddNumber = sumOddNumber + array[i];    
 }
-Console.WriteLine($"Сумма элементов массива на нечетных позициях = {sumEven}");
+Console.WriteLine($"Сумма элементов массива на нечетных позициях = {sumOddNumber}");

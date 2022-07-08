b9890a7c947a7c158cd449b44cba6183534e4a2d
Evgeniy <241126@mail.ru>
2022-07-08 13:24:08 +0300

    Example_006

---
+++ Example_006/Program.cs
@@ -0,0 +1,29 @@
+﻿int[] array = new int[12];
+int sumPositive = 0;
+int sumNegative = 0;
+
+
+for (int i=0; i< array.Length; i++)
+{
+    array[i] = new Random(). Next(-9, 10);
+}
+
+void PrintArray(int[] array)
+{
+    for (int i=0; i< array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+PrintArray(array);
+
+for (int i=0; i< array.Length; i++)
+{
+    if(array[i] > 0)
+        sumPositive += array[i];
+    else 
+        sumNegative += array[i];    
+}
+Console.WriteLine($"Сумма положительных = {sumPositive}, сумма отрицательных = {sumNegative}");

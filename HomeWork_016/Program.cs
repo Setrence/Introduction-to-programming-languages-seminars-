9082322f9c9918bc92de4ef3eabf389ffdf216cc
Evgeniy <241126@mail.ru>
2022-07-08 15:45:31 +0300

    HomeWork_016

--- HomeWork_016/Program.cs
+++ HomeWork_016/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+int[] array = new int[4];
+int max = 0;
+int min = array[0];
+int substr = 0;
+
+for (int i=0; i< array.Length; i++)
+{
+    array[i] = new Random(). Next(-100 , 100);
+}
+
+PrintArray(array);
+
+void PrintArray(int[] array)
+{
+    for (int i=0; i< array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+void Max (int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (max < array[i])
+        {
+            max = array[i]; 
+        }
+        else max = max;
+    }
+    Console.WriteLine($"max = {max}");
+}
+
+void Min (int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (min >= array[i])
+        {
+            min = array[i]; 
+        }
+        else min = min;
+    }
+    Console.WriteLine($"min = {min}");
+}
+
+Max(array);
+Min(array);
+
+substr = max - min;
+Console.WriteLine($"Разность = {substr}");

a8cb7d42edf6b1749b863a1e1d855271868f818a
Evgeniy <241126@mail.ru>
2022-07-14 18:00:57 +0300

    HomeWork_017

---
+++ HomeWork_017/Program.cs
@@ -0,0 +1,33 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры числа через запятую. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write("Введите число через запятую: ");
+string[] a = Console.ReadLine().Split(",");
+int[] b = Converter(a);
+int counter = Count(b);
+
+Console.WriteLine(counter);
+
+static int Count(int[] array)
+{
+    int returnCounter = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > 0)
+        {
+            returnCounter ++;
+        }
+    }
+
+    return returnCounter;
+}
+
+static int[] Converter(string[] array)
+{
+    int[] returnArray = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+       returnArray[i] = Convert.ToInt32(array[i]);
+    }
+
+    return returnArray;
+}
